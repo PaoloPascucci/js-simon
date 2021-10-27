9218d65476ee5988b510bd9cf683f4d1e91126e2
PaoloPascucci <pascuccip9@gmail.com>
2021-10-27 16:59:41 +0200

    Add Esercizio Completo

--- main.js
+++ main.js
@@ -10,3 +10,49 @@
 //     Info:
 // L'alert non si chiude in automatico. Dovete chiuderla voi a mano cliccando su ok.
 
+// funzione numeri random
+function RanNum() {
+    return Math.floor((Math.random() * 100) + 1)
+}
+//array numeri random, pusha i numeri nell'array
+const RanNumArr = [];
+// for (let i = 0; i < 5; i++) {
+//     RanNumArr.push(RanNum());
+// }
+while (RanNumArr.length < 5) {
+    var numero = RanNum();
+    if (RanNumArr.includes(numero) == false) {
+        RanNumArr.push(numero)
+    }
+
+}
+//mostra l'array
+console.log(RanNumArr);
+
+alert(RanNumArr)
+
+let ExactNumber = [];
+let Numbers = [];
+
+setTimeout(function () {
+    for (let index = 0; index < RanNumArr.length; index++) {
+        var Usernumber = parseInt(prompt('Inserisci i numeri che ti ricordi'));
+        if (!Numbers.includes(Usernumber)) {
+            Numbers.push(Usernumber);
+            if (RanNumArr.includes(Usernumber) == true) {
+                ExactNumber.push(Usernumber)
+
+            }
+        }
+    }
+    document.querySelector('.conatiner').innerHTML = InsUser()
+}
+
+    , 5000);
+
+
+function InsUser() {
+    return `
+    <div> hai indovinato ${ExactNumber.length} numeri: ${ExactNumber}</div>
+    `
+}
